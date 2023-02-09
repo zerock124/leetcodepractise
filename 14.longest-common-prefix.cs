955e29510931d2511d123d5f95bb83d0abf73d5e
xinyu <zerock124@gmail.com>
2023-02-09 12:10:32 +0800

    14題

--- 14.longest-common-prefix.cs
+++ 14.longest-common-prefix.cs
@@ -5,9 +5,30 @@
  */
 
 // @lc code=start
-public class Solution {
-    public string LongestCommonPrefix(string[] strs) {
-        
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs.Length == 1)
+            return strs[0];
+
+        string _outPut = "";
+
+        //如果沒有資料 或是 第一筆的長度為0
+        if (strs == null || strs[1].Length == 0)
+            return _outPut;
+
+        //逐筆比對
+        string _strFirst = strs[0];
+        string[] _strList = strs.Skip(1).ToArray();
+        for (int i = 0; i < _strFirst.Length; i++)
+        {
+            if (_strList.All(x => x.Substring(0, x.Length > i + 1 ? i + 1 : x.Length) == _outPut + _strFirst[i]) == true)
+                _outPut = _outPut + _strFirst[i];
+            else
+                break;
+        }
+        return _outPut;
     }
 }
 // @lc code=end
